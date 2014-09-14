f9604970f6b85d5040b44b5d6bf4e6c1aa5c2d73
Ben Komalo <benkomalo@gmail.com>
2014-09-14 13:02:13 -0700

    Inline images

--- app/assets/javascripts/process-terms.js
+++ app/assets/javascripts/process-terms.js
@@ -4,29 +4,40 @@ var NEW_LINE = /\n/g;
 
 var DUMB_URL_RE = /(https?:\/\/[^\s]+)/g;
 
-function processText(textContent) {
-  var sanitized = slowEscape(textContent);
-
-  DUMB_URL_RE.lastIndex = 0;
-  var results = []
-  var i = 0;
-  var match;
-  while (match = DUMB_URL_RE.exec(sanitized)) {
-    var l = match[0].length;
-    var safeMatch = slowEscape(match[0]);
-    results.push(
-        sanitized.substring(i, DUMB_URL_RE.lastIndex - l),
-        '<a href="' + safeMatch + '">' + safeMatch + '</a>');
-    i = DUMB_URL_RE.lastIndex;
-  }
-  results.push(sanitized.substring(i));
-  return results.join('').replace(NEW_LINE, '<br>');
-}
-
 var $div = $('<div/>');
 
 function slowEscape(s) {
   return $div.text(s).html();
+}
+
+function buildLink(urlText) {
+  var safeText = slowEscape(urlText);
+  var l = safeText.length;
+  if (safeText.lastIndexOf('.png') === l - 4 ||
+      safeText.lastIndexOf('.jpg') === l - 4 ||
+      safeText.lastIndexOf('.gif') === l - 4) {
+    return '<img src="' + safeText + '">';
+  } else {
+    return '<a href="' + safeText + '">' + safeText + '</a>';
+  }
+}
+
+function processText(textContent) {
+  var sanitized = slowEscape(textContent);
+
+  DUMB_URL_RE.lastIndex = 0;
+  var results = [];
+  var i = 0;
+  var match;
+  while (match = DUMB_URL_RE.exec(sanitized)) {
+    var l = match[0].length;
+    results.push(
+        sanitized.substring(i, DUMB_URL_RE.lastIndex - l),
+        buildLink(match[0]));
+    i = DUMB_URL_RE.lastIndex;
+  }
+  results.push(sanitized.substring(i));
+  return results.join('').replace(NEW_LINE, '<br>');
 }
 
 function processTermDefinition(el) {
